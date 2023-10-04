db75dc954ecd880a14b44fc40930c154aed96a64
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2023-10-04 20:51:45 +0530

    added the specific logger

--- master_test_file.py
+++ master_test_file.py
@@ -1,9 +1,16 @@
 from flask import Flask,jsonify
 from flask import request,has_request_context
 
+import websocket
+import json
+
 import logging
 from logging.handlers import TimedRotatingFileHandler
-logging.basicConfig(filename="True_binance.log",level=10,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',filemode='w')
+
+#THis is the basic configuration of the logging models
+
+
+logging.basicConfig(filename=f"app_test.log",level=10,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',filemode='w')
 
 #create flask application
 
@@ -20,7 +27,7 @@ class NewFormatter(logging.Formatter):
             record.remote=None
         return super().format(record)
 
-logFormatter=logging.Formatter('%(asctime)s- %(message)s - %(url)s - %(remote)')
+logFormatter=logging.Formatter('%(asctime)s - %(url)s - %(remote) - %(message)s')
 
 #add console handler to the root logger
 consoleHandler=logging.StreamHandler()
@@ -28,29 +35,29 @@ consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
 
 #add file handler to the root logger
-fileHandler= TimedRotatingFileHandler(filename='True_binance.log',when="midnight", interval=1 ,backupCount=7)
+fileHandler= TimedRotatingFileHandler(filename=f'app_test.log',when="midnight", interval=1 ,backupCount=7)
 fileHandler.setFormatter(logFormatter)
 logger.addHandler(fileHandler)
 
+#types of the logs the system is generating.
 logging.info
 logging.error
 logging.warning
 logging.critical
 
+#This is the part of the base app server using flask.
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     app.logger.info("from route handler..")
-    return "hello world!!!"
+    return "application start"
 
-import websocket
-import json
+#This is where the base websocket server is embedded ito the application.
 
 def on_open(ws):
     print("Opened connection to the stream ")
     logger.debug("this is the stream arriving into the log")
-    print(ws,logging.debug)
     try:
         subscribe={ "method": "SUBSCRIBE", "params": ["btcusdt@aggTrade"] , "id": 1}
         ws.send(json.dumps(subscribe))
@@ -60,13 +67,11 @@ def on_open(ws):
 def on_message(ws, message):
     data=json.loads(message)
     app.logger.info("program is working as expected.")
-    print(data,logger.info)
     print(message)
 
 def on_error(ws, error):
     app.logger.error("The program encountered an error")
     logger.warning("Warning, the program may not function properly")
-    print(error,logging.error,logging.warning)
 
 def on_close(ws, close_status_code, close_msg):
     app.logger.critical("the connection was lost")
@@ -79,21 +84,20 @@ url="wss://stream.binance.com:9443/ws/btcusdt@aggTrade"
 ws = websocket.WebSocketApp(url,on_open=on_open,on_message=on_message,
                             on_error=on_error,on_close=on_close)
 ws.run_forever()
-import requests
-def get_last_price():
-    response = requests.get('http://localhost:5000/ltp')
-    data = response.json()
-    if 'last price' in data:
-        return data['last price']
-    else:
-        return None
-    if __name__ == "__main__":
-        while True:
-            Ltp = get_last_price()
-            if ltp is not None:
-                print("Last Traded Price. (ltp)")
-            else:
-                print("No data available")
-            input("press enter to fetch again")
+#import requests
+#def get_last_price():
+#    response = requests.get('http://localhost:5000/ltp')
+#   data = response.json()
+#    if 'last price' in data:
+##
+#    else:
+#        return None
+##    if __name__ == "__main__":
+#        while True:
+#            Ltp = get_last_price()
+#            if ltp is not None:
+#                print("Last Traded Price. (ltp)")
+#3\               print("No data available")
+#           input("press enter to fetch again")
 #run the flask application
 app.run(host="0.0.0.0",port=50100,debug=True)
