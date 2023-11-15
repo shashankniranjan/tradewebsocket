204e74fa782fcd39fe7adf9eeff1edee840cea40
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2023-11-15 18:42:25 +0530

    updates in applications
    added the feature of creating individual folders for all the logs and intermediate folders for both spot and perpetual data.

--- .idea/historical_data_saver/Application_spot.py
+++ .idea/historical_data_saver/Application_spot.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask,jsonify,make_response
 import datetime
 import websocket
@@ -7,27 +8,32 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import time
 import threading
+import pytz
+import os
 
 # Create a Flask application instance
 app = Flask(__name__)
 # Create a logger object for the current module
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(msecs)d - %(message)s')
-file_handler = TimedRotatingFileHandler("Application_spot_logs/App_Main_spot_logs.log", when="midnight", interval=1, backupCount=None)# Import the TimedRotatingFileHandler class from the logging module# Create a TimedRotatingFileHandler object
-file_handler.setLevel(logging.INFO) # Set the logging level for the logger object
-file_handler.setFormatter(logging.Formatter("%(levelname)s - %(msecs)d - %(message)s"))# Set the formatter for the handler
-logger.addHandler(file_handler)# Add the handler to the logge
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(msecs)d - %(message)s')
+
+file_handler = TimedRotatingFileHandler("Application_perpetual_logs/App_Main_perpetual_logs.log", when="midnight", interval=1, backupCount=None)# Import the TimedRotatingFileHandler class from the logging module# Create a TimedRotatingFileHandler object
+file_handler.setLevel(logging.INFO) # Set the logging level for the logger object
+file_handler.setFormatter(logging.Formatter(f"%(levelname)s - %(msecs)d - %(message)s"))# Set the formatter for the handler
+logger.addHandler(file_handler)# Add the handler to the logger
 
 # Initialize variables
 last_price = None
 last_update_time = None
 response = None
 symbol = None
+currency_pair=None
+currency_pairs=None
 
 @app.route('/ltp')#returns last price for the given asset
 #Decorates the get_last_price() function with the route() decorator.
-# This decorator registers the get_last_price() function as a handler for the /ltp route.
+# This decorator regutcers the get_last_price() function as a handler for the /ltp route.
 def get_last_price():
     global last_price, last_update_time, response, trade_data, symbol
     #Declares the last_price, last_update_time, response, trade_data, and symbol variables as
@@ -66,12 +72,14 @@ def get_last_price():
                 # 200 means success
             return response
             # Returns the response object.
+
         else:
             response_data = {"message": "No recent data available"}
             logger.error(f"last_price -symbol- {last_price} - {symbol}")
             logger.error("message - No recent data available")
             response = make_response(jsonify(response_data), 404)
             return response
+
 
 @app.route('/ltm')
 def get_last_msg():
@@ -86,7 +94,7 @@ def startWebSocket(currency_pair):
 #This line creates a while True loop.
 #  This means that the code will keep trying to start the websocket until it is successful.
         try:
-            url = f"wss://stream.binance.com:9443/ws/{currency_pair}@aggTrade"
+            url = f"wss://fstream.binance.com:/ws/{currency_pair}@aggTrade"
             ws = websocket.WebSocketApp(url,#The URL of the Binance websocket server.
                                         on_open=on_open,#A callback function that is called when the websocket is opened.
                                         on_message=on_message,#A callback function that is called when the websocket receives a message.
@@ -109,35 +117,68 @@ def on_open(ws):
 
 #this is the part which takes the messsage from the application
 def on_message(ws, message):
-    global response
-    #Declares the response variable as a global variable.
-    #This means that the response variable can be accessed from within the on_message() function.
+    global response, symbol
+
+    # Declares the response variable as a global variable.
+    # This means that the response variable can be accessed from within the on_message() function.
     response = message
-    #Assigns the value of the message parameter to the response variable.
-    #this adds the data to the logger as well as adds the required data to the stream
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    #ets the current time and formats it as a string
-    logger.info(f"{current_time} - {message}")
-    print(current_time," ",message)
-    
+
+    # Assigns the value of the message parameter to the response variable.
+    try:
+        # Convert the message to a JSON object
+        json_data = json.loads(message)
+
+        # Extract the symbol from the JSON object
+        symbol = json_data["s"]
+
+        timezone = pytz.timezone('UTC')
+        current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
+        timezone_offset = datetime.datetime.now(timezone).utcoffset()
+        logger.info(f"{current_time} {timezone} {timezone_offset} - {message}")
+        print(current_time, " ", message)
+
+        # Create a symbol-specific log file path
+        symbol_file_path = f"currency_logs/Spot_currency_logs/{symbol}/{symbol}_spot_logs.log"
+
+        # Check if the symbol-specific log file exists
+        if not os.path.exists(symbol_file_path):
+            # Create the symbol-specific log file if it doesn't exist
+            with open(symbol_file_path, "w") as f:
+                f.write("")
+
+        # Open the symbol-specific log file in append mode
+        with open(symbol_file_path, "a") as f:
+            # Write the message to the symbol-specific log file
+            f.write(f"{current_time} {message}\n")
+
+        # Log the message to the main log file
+        logger.info(f"{current_time} {timezone} {timezone_offset} - {message}\n")
+    except Exception as e:
+        # Handle any errors that occur during processing
+        logger.error(f"Error processing message: {e}")
+
+
 #this takes the part of the error to the logs and from the data stream
 def on_error(ws, error):
     #Logs a message to the error log indicating that the program encountered an error.
     logger.error("The program encountered an error")
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    logger.error(f"{current_time} - {error} - {response}")
+    timezone = pytz.timezone('UTC')
+    current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
+    timezone_offset = datetime.datetime.now(timezone).utcoffset()
+    logger.error(f"{current_time} {timezone} {timezone_offset} - {error} - {response}")
     # logger.error(response)
     time.sleep(1)  # Wait for 1 seconds before resubscribing
-    ws.close() # Close the existing WebSocket connection
+    ws.close() # Close the exutcing WebSocket connection
     startWebSocket(currency_pair)
     #statement starts a new WebSocket connection. This is necessary because the old WebSocket connection cannot be reused.
     # Reconnect and resubscribe
 
 def on_close(ws, close_status_code, close_msg):
     """Logs a critical message indicating that the connection was lost and restarts the websocket connection if the connection was closed due to an error."""
-
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    logger.critical(f"{current_time} - Critical error: {close_msg}")
+    timezone = pytz.timezone('UTC')
+    current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
+    timezone_offset = datetime.datetime.now(timezone).utcoffset()
+    logger.critical(f"{current_time} {timezone} {timezone_offset} - Critical error: {close_msg}")
 
     if close_status_code != 1000:
         logger.error(f"Connection closed due to error: {close_msg}")
@@ -147,8 +188,6 @@ def on_close(ws, close_status_code, close_msg):
 
 def start_websocket_thread(currency_pair):
     try:
-
-
             thread = threading.Thread(target=startWebSocket, args=(currency_pair,))
             #Creates a new Thread object.
             #Specifies the startWebSocket() function as the target function for the thread.
@@ -166,14 +205,37 @@ def start_websocket_thread(currency_pair):
             logger.error(f"Error starting websocket thread for {currency_pair}: {e}")
             #Starts the thread.
 
+def create_directories_for_each_currency(currency_pairs, folder_path):
+    global response
+
+    # Declares the response variable as a global variable.
+    # This means that the response variable can be accessed from within the on_message() function.
+    response = message
+
+    # Creates a directory for each currency in the list of currency pairs.
+    for currency_pair in currency_pairs:
+        if currency_pair:
+            directory_name = currency_pair.split("/")[0]
+            full_path = os.path.join(folder_path, directory_name)
+
+            # Create a new logger for each thread
+            thread_logger = logging.getLogger(f"thread_{currency_pair}")
+
+            # Use the thread-specific logger to create the directory
+            try:
+                os.makedirs(full_path, exist_ok=True)
+                thread_logger.info(f"Created directory for currency pair: {currency_pair}")
+            except Exception as e:
+                thread_logger.error(f"Failed to create directory for currency pair: {currency_pair} - {e}")
+
 
 def startServer():
     """Starts the server on port 5000."""
     logger.info("Inside startServer()")
 
-    # Try to run the app on port 5000.
+    # Try to run the app on port 4000.
     try:
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=4000)
 
     # If an error occurs, log it.
     except Exception as error:
@@ -187,24 +249,28 @@ if __name__ == "__main__":
         server_thread.start()
 
         # Load the currency pairs from the configuration file
-        with open("configuration_spot_file.conf", "r") as f:
+        with open("configuration_files/configuration_perpetual_file.conf", "r") as f:
             currency_pairs = f.read().splitlines()
 
-        # Create an empty list to store the websocket threads
+        # Create an empty lutc to store the websocket threads
         threads = []
 
-        # Iterate over the list of currency pairs and start a new websocket thread for each currency pair
+        # Iterate over the lutc of currency pairs and start a new websocket thread for each currency pair
         for currency_pair in currency_pairs:
             thread = start_websocket_thread(currency_pair)
-
-            # If the websocket thread was successfully created and started, add it to the list
+        
+            # If the websocket thread was successfully created and started, add it to the lutc
             if thread is not None:
                 threads.append(thread)
 
+        # Create a directory for each currency in the list of currency pairs.
+        folder_path = f"currency_logs/Spot_currency_logs"
+        create_directories_for_each_currency(currency_pairs, folder_path)
+
     finally:
         # Wait for all of the websocket threads to finish running
-        #r thread in threads:
                 thread.join()
 
         # If the program closed unexpectedly, log an error message
                 logger.error("the program closed unexpectedly")
+
