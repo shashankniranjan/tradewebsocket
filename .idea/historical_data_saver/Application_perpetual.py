e8cf18e8ddb69c7d3518270dcbfbd3c3d2028b62
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2023-11-21 21:36:55 +0530

    updated with 5second restart socket feature.

--- .idea/historical_data_saver/Application_perpetual.py
+++ .idea/historical_data_saver/Application_perpetual.py
@@ -1,4 +1,3 @@
-from email import message
 from flask import Flask,jsonify,make_response
 import datetime
 import websocket
@@ -8,7 +7,7 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import time
 import threading
-import pytz
+import time
 import os
 
 # Create a Flask application instance
@@ -94,7 +93,7 @@ def startWebSocket(currency_pair):
 #This line creates a while True loop.
 #  This means that the code will keep trying to start the websocket until it is successful.
         try:
-            url = f"wss://fstream.binance.com:/ws/{currency_pair}@aggTrade"
+            url = f"wss://fstream.binance.com:/ws/{currency_pair}@ticker"
             ws = websocket.WebSocketApp(url,#The URL of the Binance websocket server.
                                         on_open=on_open,#A callback function that is called when the websocket is opened.
                                         on_message=on_message,#A callback function that is called when the websocket receives a message.
@@ -115,82 +114,69 @@ def on_open(ws):
     logger.debug("this is the stream arriving into the log")
     # Logs a message to the debug log indicating that the websocket has been opened.
 
-#this is the part which takes the messsage from the application
 def on_message(ws, message):
-    global response, symbol,last_price
+    global response, symbol, last_price, last_message_time
 
-    # Declares the response variable as a global variable.
-    # This means that the response variable can be accessed from within the on_message() function.
     response = message
 
-    # Assigns the value of the message parameter to the response variable.
     try:
-        # Convert the message to a JSON object
         json_data = json.loads(message)
-
-        # Extract the symbol from the JSON object
         symbol = json_data["s"]
 
-        timezone = pytz.timezone('UTC')
-        current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
-        timezone_offset = datetime.datetime.now(timezone).utcoffset()
-        logger.info(f"{current_time} {timezone} {timezone_offset} - {message}")
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%z')
+        logger.info(f"{current_time} - {message}")
         print(current_time, " ", message)
 
-        # Create a symbol-specific log file handler
-        symbol_logger = logging.getLogger(f"perpetual_currency_logs.{symbol}")
+        symbol_logger = logging.getLogger(f"Spot_currency_logs.{symbol}")
         symbol_log_handler = TimedRotatingFileHandler(
             f"currency_logs/Perpetual_currency_logs/{symbol}/{symbol}_perpetual_logs.log",
             when="midnight",
             interval=1,
             backupCount=10000000000000
         )
-        symbol_log_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)-8s %(message)s"))
+        symbol_log_handler.setFormatter(logging.Formatter(f"%(msecs)d - %(message)s"))
         symbol_logger.addHandler(symbol_log_handler)
 
-        # Log the message with timestamp and timezone info
-        symbol_logger.info(f"{current_time} {timezone} {timezone_offset} - {message}")
+        symbol_logger.info(f"{current_time} - {message}")
 
-        # Log the message to the main log file
-        logger.info(f"{current_time} {timezone} {timezone_offset} - {message}")
+        last_message_time = time.time()
+
+        if 'p' in json_data:
+            last_price = float(json_data['p'])
+            symbol = json_data['s']
+            last_update_time = time.time()
+            current_time = time.time()
+
+            if last_price is not None and last_update_time is not None and current_time - last_update_time <= 5:
+                response_data = {
+                    "last_price": last_price,
+                    "symbol": symbol,
+                }
+            # logger.info(f"last_price -symbol- {last_price} - {symbol}")
+            # response = make_response(jsonify(response_data), 200)
+            # return response
+
+            else:
+                response_data = {"message": "No recent data available"}
+                logger.error(f"last_price -symbol- {last_price} - {symbol}")
+                logger.error("message - No recent data available")
+            #  response = make_response(jsonify(response_data), 404)
+            # return response
+
     except Exception as e:
-        # Handle any errors that occur during processing
-            logger.error(f"Error processing message: {e}")
-        
+        logger.error(f"Error processing message: {e}")
+        if time.time() - last_message_time > 5:
+            ws.close()
+            startWebSocket(currency_pair)
+            last_message_time = time.time()
 
-    # last_update_time = time.time()
-    # #Assigns the current time to the current_time variable. The time.time() function is used to get the current time.
-    # current_time = time.time()
-
-    # if last_price is not None and last_update_time is not None and current_time - last_update_time <= 5:
-    #     # Checks if the last_price variable is not None, the last_update_time variable is not None,
-    #     #  and the difference between the current time and the last update time is less than or equal to 5 seconds.
-    #     response_data = {
-    #         "last_price": last_price,
-    #         "symbol": symbol,
-    #     }
-    #     logger.info(f"last_price -symbol- {last_price} - {symbol}")
-    #     response = make_response(jsonify(response_data), 200)
-    #     # Creates a response object with the response_data dictionary and a status code of 200.
-    #         # 200 means success
-    #     return response
-    #     # Returns the response object.
-
-    # else:
-    #     response_data = {"message": "No recent data available"}
-    #     logger.error(f"last_price -symbol- {last_price} - {symbol}")
-    #     logger.error("message - No recent data available")
-    #     response = make_response(jsonify(response_data), 404)
-    #     return response
 
 #this takes the part of the error to the logs and from the data stream
 def on_error(ws, error):
     #Logs a message to the error log indicating that the program encountered an error.
     logger.error("The program encountered an error")
-    timezone = pytz.timezone('UTC')
-    current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
-    timezone_offset = datetime.datetime.now(timezone).utcoffset()
-    logger.error(f"{current_time} {timezone} {timezone_offset} - {error} - {response}")
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S- %z')
+    logger.error(f"{current_time} - {error} - {response}")
     # logger.error(response)
     time.sleep(1)  # Wait for 1 seconds before resubscribing
     ws.close() # Close the exutcing WebSocket connection
@@ -200,10 +186,8 @@ def on_error(ws, error):
 
 def on_close(ws, close_status_code, close_msg):
     """Logs a critical message indicating that the connection was lost and restarts the websocket connection if the connection was closed due to an error."""
-    timezone = pytz.timezone('UTC')
-    current_time = datetime.datetime.now(timezone).strftime('%Y-%m-%d_%H-%M-%S-%z')
-    timezone_offset = datetime.datetime.now(timezone).utcoffset()
-    logger.critical(f"{current_time} {timezone} {timezone_offset} - Critical error: {close_msg}")
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S- %z')
+    logger.critical(f"{current_time} - Critical error: {close_msg}")
 
     if close_status_code != 1000:
         logger.error(f"Connection closed due to error: {close_msg}")
@@ -231,11 +215,6 @@ def start_websocket_thread(currency_pair):
             #Starts the thread.
 
 def create_directories_for_each_currency(currency_pairs, folder_path):
-    global response
-
-    # Declares the response variable as a global variable.
-    # This means that the response variable can be accessed from within the on_message() function.
-    response = message
 
     # Creates a directory for each currency in the list of currency pairs.
     for currency_pair in currency_pairs:
@@ -277,14 +256,14 @@ if __name__ == "__main__":
         with open("configuration_files/configuration_perpetual_file.conf", "r") as f:
             currency_pairs = f.read().splitlines()
 
-        # Create an empty lutc to store the websocket threads
+        # Create an empty list to store the websocket threads
         threads = []
 
-        # Iterate over the lutc of currency pairs and start a new websocket thread for each currency pair
+        # Iterate over the list of currency pairs and start a new websocket thread for each currency pair
         for currency_pair in currency_pairs:
             thread = start_websocket_thread(currency_pair)
-        
-            # If the websocket thread was successfully created and started, add it to the lutc
+
+            # If the websocket thread was successfully created and started, add it to the list
             if thread is not None:
                 threads.append(thread)
 
@@ -293,9 +272,11 @@ if __name__ == "__main__":
         create_directories_for_each_currency(currency_pairs, folder_path)
 
     finally:
-        # Wait for all of the websocket threads to finish running
-                thread.join()
+        for thread in threads:
+            # Wait for all of the websocket threads to finish running
+            thread.join()
 
         # If the program closed unexpectedly, log an error message
-                logger.error("the program closed unexpectedly")
+        logger.error("the program closed unexpectedly")
+
 
